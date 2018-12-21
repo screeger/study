2e62dcc8a5a4bdf0ed6e9e1f7def491785f2b564
screeger <45769792+screeger@users.noreply.github.com>
2018-12-20 20:09:36 -0600

    Sam started entering S1

--- src/scenarios/scene1/page3.js
+++ src/scenarios/scene1/page3.js
@@ -5,8 +5,13 @@ export default (props) => {
   return (
     <React.Fragment>
       <PillNavigation active={3} navRequested={props.getNextPage} />
-      <h1>This is Scene 1 - Page Three</h1>
-      <p>All stuff in this area talks about page three things.</p>
+      <h1>Design an online platform that plays music</h1>
+      <h2>Option 1</h2>
+      <p>blah blah</p>
+      <h2>Option 2</h2>
+      <p>blah blah</p>
+      <h3>Option 3</h3>
+      <p>All stuff in this area talks about page one things.</p>
     </React.Fragment>
   );
 };
