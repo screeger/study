48d2fca96b8c6774de4fecec60c2d5c29528554a
screeger <45769792+screeger@users.noreply.github.com>
2018-12-26 12:31:56 -0600

    Groups

--- src/helpers/constants.js
+++ src/helpers/constants.js
@@ -5,50 +5,62 @@ export const SCENARIOS = [
   { retain: "leave this here" },
   {
     name: "Describe the Characteristics and Needs of the Stakeholders",
-    color: "#870230"
+    color: "#870230",
+    group: "a"
   },
   {
     name: "Substitute the Primary Stakeholder with Another Stakeholder",
-    color: "#BA5B3F"
+    color: "#BA5B3F",
+    group: "b"
   },
   {
     name: "Describe Cultural Implications",
-    color: "#2D5DB2"
+    color: "#2D5DB2",
+    group: "c"
   },
   {
     name: "Rely on Existing Solutions",
-    color: "#1A3769"
+    color: "#1A3769",
+    group: "d"
   },
   {
     name: "Describe Visual Attributes",
-    color: "#9C6525"
+    color: "#9C6525",
+    group: "b"
   },
   {
     name: "Describe the Context",
-    color: "#9C5212"
+    color: "#9C5212",
+    group: "a"
   },
   {
     name: "Describe the Users’ Interaction",
-    color: "#5D74A8"
+    color: "#5D74A8",
+    group: "d"
   },
   {
     name: "Describe the Functionality",
-    color: "#4D818C"
+    color: "#4D818C",
+    group: "c"
   },
   {
     name: "Examine Assumptions",
-    color: "#165F69"
+    color: "#165F69",
+    group: "b"
   },
   {
     name: "Determine the Underlying Issue",
-    color: "#C48A19"
+    color: "#C48A19",
+    group: "a"
   },
   {
     name: "Describe Mobility Characteristics",
-    color: "#960E0D"
+    color: "#960E0D",
+    group: "d"
   },
   {
     name: "Describe Maintenance Needs",
-    color: "#A63737"
+    color: "#A63737",
+    group: "c"
   }
 ];
