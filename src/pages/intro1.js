9e4398d2c8e9d6b483771e846db28008047f0066
screeger <45769792+screeger@users.noreply.github.com>
2018-12-26 23:24:53 -0600

    Compass Edits

--- src/pages/intro1.js
+++ src/pages/intro1.js
@@ -7,7 +7,12 @@ export default (props) => {
 			<h1>Problem Solution Co-Evolution</h1>
 			<p>Within the problem generation space, there are multiple alternative views to reframe a problem statement.
 				This exercise is to help view your problem in different ways that you may not have considered.
-				Our 22 strategies will help you uncover new waysto develop your problem and implement with the solution.</p>
+				The 12 strategies will help you uncover new ways to develop your problem and implement with the solution.</p>
+			<p>For this study, you will receive 3 random strategies using a particular scenario. The paper and pen you received
+				is to help you think through ideas.
+
+			</p>
+			<p>If you have any questions during this time, please ask Sammy.</p>
 			<NextPage gotoPage={() => {
 				props.goNextPage('intro2');
 			}}>Continue</NextPage>
--- src/pages/intro2.js
+++ src/pages/intro2.js
@@ -8,6 +8,7 @@ export default (props) => {
 			<p>In the front end of the design process, there may be many paths of potential problems you can take.
 				While it canbe a bit overwhelming, we are here to help you navigate your route by providing strategies
 				that can ultimately steer your problem in the right direction.</p>
+			<p>Follow the instructions on each page as you discover each new strategy.</p>
 			<NextPage gotoPage={() => {
 				props.goNextPage('problemScenario');
 			}}>Continue</NextPage>
