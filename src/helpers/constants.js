4ec42967aa025a9c982c59b82ee74258c913bf2b
Craig Creeger <craig@pixelpro.biz>
2018-12-27 00:07:25 -0600

    Updated names

--- src/helpers/constants.js
+++ src/helpers/constants.js
@@ -3,62 +3,62 @@ export const FREE_ROTATIONS = 9; // Number of times the compass rotates before p
 export const SCENARIOS = [
   { retain: "leave this here" },
   {
-    name: "Describe the Characteristics and Needs of the Stakeholders",
+    name: "1 – Describe the Characteristics and Needs of the Stakeholders",
     color: "#870230",
     group: "a"
   },
   {
-    name: "Substitute the Primary Stakeholder with Another Stakeholder",
+    name: "2 – Substitute the Primary Stakeholder with Another Stakeholder",
     color: "#BA5B3F",
     group: "b"
   },
   {
-    name: "Describe Cultural Implications",
+    name: "3 – Describe Cultural Implications",
     color: "#2D5DB2",
     group: "c"
   },
   {
-    name: "Rely on Existing Solutions",
+    name: "4 – Rely on Existing Solutions",
     color: "#1A3769",
     group: "d"
   },
   {
-    name: "Describe Visual Attributes",
+    name: "5 – Describe Visual Attributes",
     color: "#9C6525",
     group: "b"
   },
   {
-    name: "Describe the Context",
+    name: "6 – Describe the Context",
     color: "#9C5212",
     group: "a"
   },
   {
-    name: "Describe the Users’ Interaction",
+    name: "7 – Describe the Users’ Interaction",
     color: "#5D74A8",
     group: "d"
   },
   {
-    name: "Describe the Functionality",
+    name: "8 – Describe the Functionality",
     color: "#4D818C",
     group: "c"
   },
   {
-    name: "Examine Assumptions",
+    name: "9 – Examine Assumptions",
     color: "#165F69",
     group: "b"
   },
   {
-    name: "Determine the Underlying Issue",
+    name: "10 – Determine the Underlying Issue",
     color: "#C48A19",
     group: "a"
   },
   {
-    name: "Describe Mobility Characteristics",
+    name: "11 – Describe Mobility Characteristics",
     color: "#960E0D",
     group: "d"
   },
   {
-    name: "Describe Maintenance Needs",
+    name: "12 – Describe Maintenance Needs",
     color: "#A63737",
     group: "c"
   }
