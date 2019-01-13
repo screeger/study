95c65e24df011480add5df175b68512b11812d0f
Craig Creeger <craig@pixelpro.biz>
2019-01-13 11:50:22 -0600

    Puts new bkgd image on page and styles the first button

--- src/pages/intro1.js
+++ src/pages/intro1.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { setBkgd, removeBkgd } from '../helpers/pageSetup';
-import bkgdUrl from "../images/park-design-small.jpg";
+import bkgdUrl from "../images/RiverBackground.jpg";
 
 class intro1 extends React.Component {
   componentDidMount () {
@@ -13,21 +13,42 @@ class intro1 extends React.Component {
     return (
       <main>
         <div style={{ textAlign: "center", paddingTop: "150px" }}>
-          <h2>Problem Solution Co-Evolution</h2>
-          <p>Strategy Generator</p>
-          <p>
-            <button
-              style={{
-                width: "8em",
-                height: "3em",
-                fontSize: "larger"
-              }}
-              onClick={() => {
-                this.props.goNextPage("intro2");
-              }}>
-              Let’s Start!
-            </button>
-          </p>
+          <div style={{
+            display: 'inline-block',
+            backgroundColor: 'rgba(255, 255, 255, 0.75)',
+            padding: '2em',
+            borderRadius: '3em'
+          }}>
+            <h2 style={{ fontSize: '2em' }}>Problem Solution Co-Evolution</h2>
+            <p>Strategy Generator</p>
+            <p style={{ marginTop: '3em' }}>
+              <button
+                style={{
+                  width: "200px",
+                  height: "3em",
+                  fontSize: "larger",
+                  position: "relative",
+                  border: 'none',
+                  backgroundColor: 'transparent',
+                  cursor: 'pointer'
+                }}
+                onClick={() => {
+                  this.props.goNextPage("intro2");
+                }}>
+                  <span style={{
+                    position: 'absolute',
+                    height: 0,
+                    width: "182px",
+                    borderBottom: "3em solid #DADADA",
+                    borderLeft: '9px solid transparent',
+                    borderRight: '9px solid transparent',
+                    top: 0,
+                    left: 0
+                  }}/>
+                <span style={{ zIndex: 1, position: 'relative', fontWeight: 'bold' }}>Let’s Start!</span>
+              </button>
+            </p>
+          </div>
         </div>
       </main>
     );
