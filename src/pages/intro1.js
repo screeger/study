ab7d6532876c15ceb01a35fcce5af4b1af1b5f95
Craig Creeger <craig@pixelpro.biz>
2019-01-13 20:55:26 -0600

    deploy 3

--- src/pages/intro1.js
+++ src/pages/intro1.js
@@ -27,26 +27,14 @@ class intro1 extends React.Component {
                   width: "200px",
                   height: "3em",
                   fontSize: "larger",
-                  position: "relative",
                   border: 'none',
-                  backgroundColor: 'transparent',
-                  cursor: 'pointer',
-                  color: 'white'
+                  backgroundColor: '#e5cb2c',
+                  color: 'black'
                 }}
                 onClick={() => {
                   this.props.goNextPage("intro2");
                 }}>
-                  <span style={{
-                    position: 'absolute',
-                    height: 0,
-                    width: "182px",
-                    borderBottom: "3em solid #7f3f00",
-                    borderLeft: '9px solid transparent',
-                    borderRight: '9px solid transparent',
-                    top: 0,
-                    left: 0
-                  }}/>
-                <span style={{ zIndex: 1, position: 'relative', fontWeight: 'bold' }}>Let’s Start!</span>
+                <span style={{ fontWeight: 'bold' }}>Let’s Start!</span>
               </button>
             </p>
           </div>
